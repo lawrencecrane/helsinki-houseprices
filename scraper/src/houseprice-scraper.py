fa310b56c492c6351931fc85d1b424832830b1f6
Lauri Kurki <lapeheku@student.jyu.fi>
2018-08-25 22:00:46 +0300

    go through pages and collect urls

--- scraper/src/houseprice-scraper.py
+++ scraper/src/houseprice-scraper.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
-oikotie_url = "https://asunnot.oikotie.fi/vuokrattavat-asunnot?cardType=101&locations=%5B%5B64,6,%22Helsinki%22%5D%5D"
 
 def get_page(url):
     options = webdriver.ChromeOptions()
@@ -20,15 +19,37 @@ def get_page(url):
 
     return html
 
+def parse_url_from_oikotie_ad(x):
+    url = x.find('a').get('href')
+    return url
+
 def parse_oikotie_page(soup):
-    house_urls = []
-    # Get element: <div class="cards"></div>
-    # This contains all the houses in the page
-    cards = soup.find('div', attrs={"class": "cards"})
-    return cards
-    #next_page = ""
-    #return house_urls, next_page
+    cards_container = soup.find('div', attrs={"class": "cards"})
+    cards = cards_container.find_all('card')
+
+    if (len(cards) == 0):
+        house_urls = None
+    else:
+        house_urls = map(parse_url_from_oikotie_ad, cards)
+
+    return house_urls
+
+def go_through_oikotie_pages():
+    oikotie_url = "https://asunnot.oikotie.fi/vuokrattavat-asunnot?cardType=101&locations=%5B%5B64,6,%22Helsinki%22%5D%5D"
+    page = "&pagination="
+    page_number = 1
+    all_urls = []
+    while True:
+        house_urls = parse_oikotie_page(get_page(oikotie_url + page + str(page_number)))
+        if (house_urls == None):
+            break
+
+        # map house_urls...
+        all_urls += house_urls
+        page_number += 1
+
+    return all_urls
+
 
 if __name__ == '__main__':
-    print(parse_oikotie_page(get_page(oikotie_url)))
-
+    print(go_through_oikotie_pages())
