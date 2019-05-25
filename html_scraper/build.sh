433d2abe1f9f1829bd6c0b65c5b3fcdb881992b5
Lauri Kurki <lauri.kurki@siili.com>
2019-05-25 22:24:41 +0300

    sqlite db

--- html_scraper/build.sh
+++ html_scraper/build.sh
@@ -1,2 +1,3 @@
+docker build -t sqlite-db-creator -f sqlite/Dockerfile .
 docker build -t seleniumserver-sidecar -f seleniumserver/Dockerfile .
 docker build -t html-scraper .
---
+++ html_scraper/sqlite/create.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sqlite3 /data/html.db < create_db.sql
