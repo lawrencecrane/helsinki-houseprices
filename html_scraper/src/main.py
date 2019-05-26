3a5d9d6700c07ec93d20967a8a08d8de3987004c
Lauri Kurki <lauri.kurki@siili.com>
2019-05-26 15:31:21 +0300

    Insert to db functionality implemented in scraper

--- html_scraper/src/main.py
+++ html_scraper/src/main.py
@@ -1,9 +1,52 @@
+import sqlite3
 import scrape_vuokraovi
+from functools import partial
 
 
-def insert_to_db(url, html):
+def main():
+    conn = sqlite3.connect('/data/html.db')
+    cur = conn.cursor()
+
+    try:
+        scrape_vuokraovi.scrape_ads_from_first_page(
+            partial(insert_to_db, cur, lambda _a, _b: None)
+        )
+
+        scrape_vuokraovi.scrape_ads_from_start_to_last_page(
+            partial(insert_to_db, cur, raise_duplicate_error),
+            2
+        )
+    except (DuplicateValueError, scrape_vuokraovi.StartEndError) as error:
+        # We just want to exit gracefully, as we have encountered a duplicate
+        # or we don't have more than 1 page
+        return
+    finally:
+        conn.close()
+
+
+def insert_to_db(conn, duplicate_callback, url, html):
+    with conn:
+        # Assume urls are unique for a year, and find if duplicate exists:
+        conn.execute("""
+        select count(1) from etuovi
+        where insert_ts > datetime(CURRENT_TIMESTAMP, '-365 days')
+        and url = ?
+        """, url)
+
+        if conn.fetchone()[0] > 0:
+            duplicate_callback(url, html)
+        else:
+            conn.execute("insert into etuovi (url, html) values (?, ?)",
+                         (url, html))
+
+
+def raise_duplicate_error(_a, _b):
+    raise DuplicateValueError
+
+
+class DuplicateValueError(Exception):
     pass
 
 
 if __name__ == '__main__':
-    scrape_vuokraovi.scrape_ads(insert_to_db)
+    main()
--- html_scraper/src/scrape_vuokraovi.py
+++ html_scraper/src/scrape_vuokraovi.py
@@ -3,21 +3,40 @@ from functools import reduce
 from bs4 import BeautifulSoup
 
 
-def scrape_ads(ad_callback):
+def scrape_ads_from_first_page(ad_callback):
     driver = scraper.create_driver()
 
+    try:
+        scrape_ads(driver, ad_callback, 1, 1)
+    finally:
+        driver.close()
+
+
+def scrape_ads_from_start_to_last_page(ad_callback, start_page=1):
+    driver = scraper.create_driver()
+
+    try:
+        page = get_page(driver, 1)
+        end_page = min(get_max_pagenumber(page), 100)
+
+        if end_page < start_page:
+            raise StartEndError
+
+        scrape_ads(driver, ad_callback, start_page, end_page)
+    finally:
+        driver.close()
+
+
+def scrape_ads(driver, ad_callback, start_page=1, end_page=1):
     scraper.do_all_urls(driver,
-                        get_all_urls(driver),
+                        get_all_urls(driver, start_page, end_page),
                         ad_callback)
 
 
-def get_all_urls(driver):
-    page = get_page(driver, 1)
-    last_page = min(get_max_pagenumber(page), 100)
-
-    return reduce(lambda a, b: a + b,
+def get_all_urls(driver, start_page=1, end_page=1):
+    return reduce(lambda acc, xs: acc + xs,
                   [get_urls_from_page(driver, page_number)
-                   for page_number in range(1, last_page)])
+                   for page_number in range(start_page, end_page)])
 
 
 def get_urls_from_page(driver, page_number):
@@ -47,3 +66,7 @@ def get_page(driver, page_number):
 def get_max_pagenumber(page):
     lis = page.find('ul', attrs={'class': 'pagination'}).find_all('li')
     return int(lis[len(lis)-2].text)
+
+
+class StartEndError(Exception):
+    pass
